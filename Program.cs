c233d887f9fed007479df5a7c756e82716d379e2
isimsek7 <isimsekmusic@gmail.com>
2024-08-07 15:43:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassObj;
+Person ogretmen1 = new Person();
+ogretmen1.Ad = "Asli";
+ogretmen1.SoyAd = "Yanik";
+ogretmen1.DogumTarihi = "13.05.1990";
+ogretmen1.Yazdir();
+
+Person ogretmen2 = new Person();
+ogretmen2.Ad = "Ali";
+ogretmen2.SoyAd = "Duran";
+ogretmen2.DogumTarihi = "23.08.1992";
+ogretmen2.Yazdir();
+
+Person ogrenci1 = new Person();
+ogrenci1.Ad = "Sinem";
+ogrenci1.SoyAd = "Sarac";
+ogrenci1.DogumTarihi = "22.09.2007";
+ogrenci1.Yazdir();
+
+Person ogrenci2 = new Person();
+ogrenci2.Ad = "Murtaza";
+ogrenci2.SoyAd = "Bakici";
+ogrenci2.DogumTarihi = "12.12.2006";
+ogrenci2.Yazdir();
+
