ece1fc067be0127917d2077f2e0f27682fa179aa
isimsek7 <isimsekmusic@gmail.com>
2024-08-08 15:23:33 +0300

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ClassObj
+{
+	public class Person
+	{
+		public string Ad;
+		public string SoyAd;
+		public string DogumTarihi;
+
+	public void Yazdir()
+		{
+			Console.WriteLine($"{DogumTarihi}'tarihi dogumlu {Ad} {SoyAd}");
+		}
+	}
+}
+	
+
+
+
+
+
+
+
+
+    // Marka, Model, Renk, Kapı Sayısı
